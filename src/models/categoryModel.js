090e7582a01b98c0eb7a72724215a0bf8ae3dbf2
Mohammad Alawad <engmohammadalawad@gmail.com>
2025-09-09 23:00:47 +0300

    delete folder models

--- src/models/categoryModel.js
+++
@@ -1,9 +0,0 @@
-import prisma from "../config/db.js";
-
-export default {
-  findAll: () => prisma.category.findMany({ orderBy: { id: "asc" } }),
-  create: (data) => prisma.category.create({ data }),
-  update: (id, data) => prisma.category.update({ where: { id }, data }),
-  remove: (id) => prisma.category.delete({ where: { id } }),
-  findById: (id) => prisma.category.findUnique({ where: { id } }),
-};
--- src/models/userModel.js
+++
@@ -1,7 +0,0 @@
-import prisma from "../config/db.js";
-
-export default {
-  create: (data) => prisma.user.create({ data }),
-  findByEmail: (email) => prisma.user.findUnique({ where: { email } }),
-  findById: (id) => prisma.user.findUnique({ where: { id } }),
-};
